490159b6d3ee81aa15795059bda6634b2d16015c
Harshit <harshit.owo@gmail.com>
2023-01-07 10:15:10 +0530

    created simple express server

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log('hi')
+const express = require('express')
+const app = express()
+const path = require('path')
+
+const templatePath = path.join(__dirname, 'views')
+
+app.set('views', templatePath)
+app.set('view engine', 'ejs')
+
+app.use(express.static('public'))
+
+app.use(express.urlencoded({ extended: false }))
+
+app.get('/', function(req, res) {
+    res.render('feeds')
+})
+
+app.listen(2000)
