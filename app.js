3dacf3b3a585cfb4fc21a8c4c6ec78f308f7757b
Harshit <harshit.owo@gmail.com>
2023-01-07 10:37:39 +0530

    did some changes

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
 const express = require('express')
+
 const app = express()
+
 const path = require('path')
 
 const templatePath = path.join(__dirname, 'views')
 
 app.set('views', templatePath)
+
 app.set('view engine', 'ejs')
 
-app.use(express.static('public'))
+app.use(express.static('myImg'))
 
 app.use(express.urlencoded({ extended: false }))
 
-app.get('/', function(req, res) {
+app.get('/feed', function(req, res) {
     res.render('feeds')
 })
 
-app.listen(2000)
+app.listen(2005)
