aaf28a4d9c59517273b909f9f7cb28b8f53e35bb
Harshit <harshit.owo@gmail.com>
2023-01-13 11:02:33 +0530

    made post page functional

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const path = require('path')
+const uuid = require('uuid')
+const fs = require('fs')
 
 const app = express()
 
 const templatePath = path.join(__dirname, 'views')
 const publicPath = path.join(__dirname, 'public')
+const dataPath = path.join(__dirname, 'data', 'articles.json')
 
 app.set('views', templatePath)
 app.set('view engine', 'ejs')
@@ -23,6 +26,24 @@ app.get('/feeds', function(req, res) {
 app.get('/post', function(req, res) {
     res.render('post')
 })
+app.get('/confirm', function(req, res) {
+    res.render('confirm')
+})
+app.post('/post', function(req, res) {
+    const article = req.body
+    article.id = uuid.v4()
+
+    const fileData = fs.readFileSync(dataPath)
+    const storedArticles = JSON.parse(fileData)
+
+    storedArticles.push(article)
+
+    fs.writeFileSync(dataPath, JSON.stringify(storedArticles))
+
+    console.log(typeof article, typeof storedArticles)
+    res.redirect('/confirm')
+})
+
 
 
 app.listen(2000)
