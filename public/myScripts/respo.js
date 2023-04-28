f3ea5754ef0cffa728bdd659ef528a409ba53d57
Harshit <harshit.owo@gmail.com>
2023-04-28 20:48:55 +0530

    added some stiff to git ignore

--- public/myScripts/respo.js
+++ public/myScripts/respo.js
@@ -1,7 +1,6 @@
 ham2Element.style.display = 'none'
 ham1Element.addEventListener('click', ham1Disable)
 ham2Element.addEventListener('click', ham2Disable)
-
 asideElement.addEventListener('click', asideDisable)
 
 for (i = 0; i < feedCount; i++) {
